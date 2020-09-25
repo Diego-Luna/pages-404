e6b69b40fd7186420220d6b3c9d1fd2966d04066
Diego Luna <diegofcolunalopez@gmail.com>
2020-09-25 10:21:51 -0500

    listo para procuducción

---
+++ precache-manifest.e453a388843cee6f76c593ee4fdd9ce2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ca05a2dc1188b104d8f36fd171d7bcd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7822b680bb5326c574b1",
+    "url": "./static/css/main.1c3254ff.chunk.css"
+  },
+  {
+    "revision": "0f897d2d9a52cc412a64",
+    "url": "./static/js/2.49ca821b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.49ca821b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7822b680bb5326c574b1",
+    "url": "./static/js/main.1afc9a78.chunk.js"
+  },
+  {
+    "revision": "21c385042c85d860459a",
+    "url": "./static/js/runtime-main.70459cf1.js"
+  },
+  {
+    "revision": "24c4d7a99da9695e6f0d4d8252ab11ac",
+    "url": "./static/media/astronauta.24c4d7a9.png"
+  },
+  {
+    "revision": "0769ef96f7ec9d2d501939ce0991c9a7",
+    "url": "./static/media/luna.0769ef96.png"
+  },
+  {
+    "revision": "bae5c1722750b6c4530857ba5d41238a",
+    "url": "./static/media/satelite.bae5c172.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7507a968cef8820345266a67564dd1ed.js"
+  "./precache-manifest.e453a388843cee6f76c593ee4fdd9ce2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
